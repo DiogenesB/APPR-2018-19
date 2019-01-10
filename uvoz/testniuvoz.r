674a8f26fe84a8abfac07b9ba639e25ff2f4a538
Tin Markon <tin.markon@stroka.si>
2019-01-10 18:50:15 +0100

    Spremenjena imena spremenljivk

--- uvoz/testniuvoz.r
+++ uvoz/testniuvoz.r
@@ -3,36 +3,39 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 #Uvoz prve tabele
-stolpci_1 <- c("Številka naročila", "Številka uporabnika", "Status naročila", "Čas nakupa", 
-               "Odobren čas nakupa", "Čas ko je pošiljko prejel partner", "Dejanski čas dostave", "Predviden čas dostave" )
+stolpci_1 <- c("ključ.naročila", "ključ.uporabnika", "status.naročila", "čas.nakupa", 
+               "odobren.čas.nakupa", "čas.ko.je.pošiljko.prejel.partner", "dejanski.čas.dostave", "predviden.čas.dostave" )
 tabela_narocil <- read_csv("podatki/olist_orders_dataset.csv", na = c("", " ", "NA")) %>% drop_na()
 colnames(tabela_narocil) <- stolpci_1
 
 
 #Uvoz druge tabele
-stolpci_2 <- c("Številka naročila", "Zaporedje plačila", "Tip plačila", "Število obrokov plačila", "Vrednost plačila")
+stolpci_2 <- c("ključ.naročila", "zaporedje.plačila", "tip.plačila", "število.obrokov.plačila", "vrednost.plačila")
 tabela_vrst_placil <- read_csv("podatki/olist_order_payments_dataset.csv")
 colnames(tabela_vrst_placil) <- stolpci_2
 
 
 #Uvoz tretje tabele
-stolpci_3 <- c("Tip produkta", "Kategorija produkta", "Dolžina naziva produkta", "Dolžina opisa produkta", 
-               "Število objavljenih fotografij produkta", "Masa produkta (g)", "Dolžina (cm)", "Višina (cm)", "Širina (cm")
+stolpci_3 <- c("ključ.izdelka", "kategorija.izdelka", "dolžina.naziva.izdelka", "dolžina.opisa.izdelka", 
+               "število.objavljenih.fotografij.izdelka", "masa.izdelka (g)", "dolžina (cm)", "višina (cm)", "širina (cm")
 tabela_produktov <- read_csv("podatki/olist_products_dataset.csv", na = c("", " ", "NA")) %>% drop_na() 
 colnames(tabela_produktov) <- stolpci_3
 
 #Uvoz četrte tabele
-stolpci_4 <- c("Original", "Prevod")
+stolpci_4 <- c("original", "prevod")
 tabela_prevodov <- read_csv("podatki/product_category_name_translation.csv")
 colnames(tabela_prevodov) <- stolpci_4
 
 
 #Združevanje 1. in 2. tabele
-narocila <- left_join(tabela_narocil, tabela_vrst_placil, by = c("Številka naročila"), copy=FALSE)
+narocila <- left_join(tabela_narocil, tabela_vrst_placil, by = c("ključ.naročila"), copy=FALSE)
+narocila$`Status naročila` <- as.factor(narocila$`Status naročila`)
 
 #Združevanje 3. in 4. tabele
-produkti <- left_join(tabela_produktov, tabela_prevodov, by = c("Kategorija produkta"="Original"), copy=FALSE)
-produkti$`Kategorija produkta` <- NULL
-names(produkti)[9] <- c("Kategorija produkta")
-
+produkti <- left_join(tabela_produktov, tabela_prevodov, by = c("kategorija.produkta"="original"), copy=FALSE)
+produkti$kategorija.izdelka <- NULL
+produkti$dolžina.naziva.izdelka <- NULL
+produkti$dolžina.opisa.izdelka<- NULL
+produkti$število.objavljenih.fotografij.izdelka <- NULL
+names(produkti)[6] <- c("kategorija.produkta")
 
