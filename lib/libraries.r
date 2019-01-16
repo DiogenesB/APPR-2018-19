c79202e385ae114425b6fc94990fd30bdffc33fa
Tin Markon <tin.markon@stroka.si>
2019-01-16 22:00:47 +0100

    Vrnjeno na prvotno stanje,

--- lib/libraries.r
+++ lib/libraries.r
@@ -4,5 +4,6 @@ library(gsubfn)
 library(reshape2)
 library(shiny)
 
+
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,6 +1,21 @@
 library(ggplot2)
 library(reshape2)
 
+meseci <- c("Januar", "Februar", "Marec", "April", "Maj", "Junij", "Julij", "Avgust", "September", "Oktober", "November", "December")
+graf_promet <- ggplot(promet_2017, aes(x = promet_2017$mesec.dostave, y = promet_2017$vrednost.placila / 1000)) + geom_col() + 
+  labs(title = "Pregled prometa v letu 2017", x = "Mesec", y = "Skupni promet (v 1000€)") + scale_x_continuous(breaks = 1:12, labels = meseci )
+
+graf_tipi <- ggplot(narocila, aes(narocila$tip.placila)) + geom_bar() +
+  labs(title = "Plačilna sredstva", x = "Tip plačilnega sredstva", y = "Število nakupov") + scale_x_discrete()
+
+graf_gostote <- ggplot(narocila, aes(narocila$vrednost.placila)) + geom_density() + scale_x_log10() + labs(title = "Porazdelitev vrednosti nakupov", x = "Vrednost plačila (log)", y = "Delež naročil")
+
+graf_december <- ggplot(december, aes(x=december$dan.narocila, y = december$vrednost.placila)) + geom_col() +
+  labs(title = "Pregled prometa v decembru 2017", x = "Dan", y = "Skupni promet") + scale_x_continuous(breaks = 1:31)
+
+graf_november <- ggplot(november, aes(x=november$dan.narocila, y=november$vrednost.placila)) + geom_col() +
+  labs(title = "Pregled prometa v novembru 2017", x = "Dan", y = "Skupni promet") + scale_x_continuous(breaks = 1:30)
+
 
 ## TODO: premakni tabele za vizualizacijo sem
 ## TODO: graf porazdelitve za vsako metodo posebej
